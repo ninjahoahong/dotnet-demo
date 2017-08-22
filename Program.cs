d51c7211726ba99a19c0d586f3353b711719c9f9
Anh Duong <ninjahoahong@users.noreply.github.com>
2017-08-22 16:01:34 +0300

    Add hello world

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+
+namespace hello_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Hello World!");
+
+			var host = new WebHostBuilder()
+			.UseKestrel()
+			.UseStartup<Startup>()
+			.Build();
+
+			host.Run();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace hello_dotnet
+{
+	public class Startup
+    {
+		public void Configure(IApplicationBuilder app)
+		{
+			app.Run(context => {
+				return context.Response.WriteAsync("Hello world");
+			});
+
+		}
+    }
+}
